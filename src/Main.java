df0b604896674ae26109df10630b75e542e07cc0
Draco Mok <my_account@AirMac-Book.EDU.YRDSB.NET>
2025-01-06 13:20:31 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Wel-a-come to a Mario's onl-a-line shopping!\nAre a you an admin or a shoppa?");
+
+    }
+}
